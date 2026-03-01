33f1edcc68ffbc9404eec8498bd060121730763d
Yaosanqi137 <zxc18023571263@outlook.com>
2026-03-02 01:48:26 +0800

    更新footer信息

--- footer.php
+++ footer.php
@@ -12,13 +12,24 @@
             <div class="footer-section">
                 <h3 data-cn="联系我们" data-en="Contact Us"></h3>
                 <ul class="footer-links">
-                    <li>Email: contact@itstudio.club</li>
+                    <li><a href="mailto:contact@itstudio.club" target="_blank" rel="noopener">Email: Contact@itstudio.club</a></li>
                     <li><a href="https://github.com/ITStudioOUC/" target="_blank" rel="noopener">GitHub</a></li>
                 </ul>
+            </div>
+
+            <div class="footer-section footer-address">
+                <h3 data-cn="办公地址" data-en="Office Address"></h3>
+                <p class="footer-address-text">
+                    <span
+                        data-cn="中国海洋大学西海岸校区信息南楼五八创新创业工坊"
+                        data-en="58 Workshop, South Information Building, West Coast Campus, Ocean University of China
+"></span>
+                </p>
             </div>
         </div>
 
         <div class="footer-bottom">
+            <a href="https://beian.miit.gov.cn/" target="_blank" rel="noopener" data-cn="桂ICP备2022004639号-2" data-en="桂ICP备2022004639号-2"></a>
             <p data-cn="&copy; <?php echo date('Y'); ?> 中国海洋大学爱特工作室. 保留所有权利." data-en="&copy; <?php echo date('Y'); ?> IT Studio, Ocean University of China. All rights reserved."></p>
         </div>
     </div>
